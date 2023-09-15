822ff446f4fe2831acc2254f90c0173fce01a75b
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-09-15 14:34:48 +0000

    UTF-8 Validation

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""0. UTF-8 Validation"""
+
+
+def validUTF8(data):
+    '''method that determines if a given data set
+    represents a valid UTF-8 encoding.
+    Args:
+        data (list[int]): data set
+    Returns:
+        True if data is a valid UTF-8 encoding,
+        else return False
+    '''
+    for i in data:
+        if i >= 255:
+            return False
+
+    return True
